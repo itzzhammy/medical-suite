f3d0caec42470610da756413504f129ef518faee
Hamza <hamza.muhammad@regentmarkets.com>
2023-10-27 14:36:22 +0800

    contact form update

--- mail/mail.php
+++ mail/mail.php
@@ -1,25 +1,59 @@
-<?
-$name = $_POST{'name'};
-$email = $_POST{'email'};
-$phone = $_POST{'phone'};
-$subject = $_POST{'subject'};
-$message = $_POST['message'];
+<?php
+$errors = [];
 
-$email_message = "
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get POST data
+    $name = isset($_POST['name']) ? strip_tags(trim($_POST['name'])) : '';
+    $email = isset($_POST['email']) ? trim($_POST['email']) : '';
+    $message = isset($_POST['message']) ? strip_tags(trim($_POST['message'])) : '';
 
-Name: ".$name."
-Email: ".$email."
-Phone: ".$phone."
-Subject: ".$subject."
-Message: ".$message."
-
+    $email_message = "
+    Name: ".$name."
+    Email: ".$email."
+    Phone: ".$phone."
+    Subject: ".$subject."
+    Message: ".$message."
 ";
 
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    // Validate form fields
+    if (empty($name)) {
+        $errors[] = 'Name is empty';
+    }
 
-mail ("humza.murtaza@gmail.com" , "New Message", $email_message, $headers );
-header("location: ../mail-success.html");
+    if (empty($email)) {
+        $errors[] = 'Email is empty';
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'Email is invalid';
+    }
+
+    if (empty($message)) {
+        $errors[] = 'Message is empty';
+    }
+
+    // If no errors, send email
+    if (empty($errors)) {
+        // Recipient email address (replace with your own)
+        $recipient = "humza.murtaza@gmail.com";
+
+        // Additional headers
+        $headers = "From: $name <$email>";
+
+        // Send email
+        if (mail($recipient, $email_message, $headers)) {
+            echo "Email sent successfully!";
+        } else {
+            echo "Failed to send email. Please try again later.";
+        }
+    } else {
+        // Display errors
+        echo "The form contains the following errors:<br>";
+        foreach ($errors as $error) {
+            echo "- $error<br>";
+        }
+    }
+} else {
+    // Not a POST request, display a 403 forbidden error
+    header("HTTP/1.1 403 Forbidden");
+    echo "You are not allowed to access this page.";
+}
 ?>
-
-
